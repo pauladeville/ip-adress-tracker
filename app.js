fda8fd1b3c353f65c435c5c57f27caff474352bf
P <paula.deville@posteo.net>
2023-04-19 10:13:33 +0200

    babel

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+//FIREBASE CONFIG
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDRpWKRNqOpvvCcCvK7_45wTO2ITxDXhmg",
+  authDomain: "ip-tracker-1bb45.firebaseapp.com",
+  projectId: "ip-tracker-1bb45",
+  storageBucket: "ip-tracker-1bb45.appspot.com",
+  messagingSenderId: "875007932485",
+  appId: "1:875007932485:web:8edd30b294d510f4e40b2c",
+  measurementId: "G-QC7D90MG5N",
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//VARIABLES
 let map;
 const tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
 });
+let ipAddress = "";
+let domainName = "";
+const submitButton = document.querySelector("#submit");
+const inputArea = document.querySelector("input");
 
 //VISITOR IP BY DEFAULT
 fetch("https://api.ipify.org/?format=json")
   .then((res) => res.json())
   .then((data) => {
-    let ipAdress = data.ip;
-    getCoordinates(ipAdress);
+    getCoordinates(data.ip);
   })
   .catch((error) => {
     console.error(error);
@@ -16,9 +35,20 @@ fetch("https://api.ipify.org/?format=json")
 
 //GET LATITUDE AND LONGITUDE
 async function getCoordinates(inputValue) {
+  if (/^([0-9]{1,3}\.){3}[0-9]{1,3}$/.test(inputValue)) {
+    ipAddress = inputValue;
+    inputArea.classList.remove("error");
+  } else if (/^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/.test(inputValue)) {
+    inputArea.classList.remove("error");
+
+    domainName = inputValue;
+  } else {
+    inputArea.classList.add("error");
+  }
   try {
-    const res = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_dfH8tDzTsnI6Fb3ulm1ilJVI7g7qd&ipAddress=" + inputValue);
+    const res = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_dfH8tDzTsnI6Fb3ulm1ilJVI7g7qd&ipAddress=" + ipAddress + "&domain=" + domainName);
     const data = await res.json();
+    updateResult(data);
     const { lat, lng } = data.location;
     updateMap(lat, lng);
   } catch (error) {
@@ -27,25 +57,34 @@ async function getCoordinates(inputValue) {
 }
 
 //UPDATE MAP
-
-async function updateMap(latitude, longitude){
-  try{
-    if(map){
-      map.remove()
+async function updateMap(latitude, longitude) {
+  try {
+    if (map) {
+      map.remove();
     }
-    // const spinner = document.querySelector("#spinner");
-    // spinner.style.display = "block";
+    const spinner = document.querySelector("#spinner");
+    spinner.style.display = "none";
     map = L.map("map").setView([latitude, longitude], 14);
     tileLayer.addTo(map);
     L.marker([latitude, longitude]).addTo(map);
-  }catch(error){
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
 }
 
-//FORM
-const submitButton = document.querySelector("#submit");
-const inputArea = document.querySelector("input");
+//UPDATE RESULT
+const addressResult = document.querySelector(".result__item--address p");
+const locationResult = document.querySelector(".result__item--location p");
+const timezoneResult = document.querySelector(".result__item--timezone p");
+const ispResult = document.querySelector(".result__item--isp p");
+async function updateResult(data) {
+  addressResult.textContent = data.ip;
+  locationResult.textContent = data.location.city;
+  timezoneResult.textContent = "UTC " + data.location.timezone;
+  ispResult.textContent = data.isp;
+}
+
+//SUBMIT FORM
 submitButton.addEventListener("click", () => {
   getCoordinates(inputArea.value);
 });
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -2,18 +2,29 @@ let map;
 const tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19
 });
+let ipAddress = "";
+let domainName = "";
+const submitButton = document.querySelector("#submit");
+const inputArea = document.querySelector("input");
 //VISITOR IP BY DEFAULT
 fetch("https://api.ipify.org/?format=json").then((res)=>res.json()).then((data)=>{
-    let ipAdress = data.ip;
-    getCoordinates(ipAdress);
+    getCoordinates(data.ip);
 }).catch((error)=>{
     console.error(error);
 });
 //GET LATITUDE AND LONGITUDE
 async function getCoordinates(inputValue) {
+    if (/^([0-9]{1,3}\.){3}[0-9]{1,3}$/.test(inputValue)) {
+        ipAddress = inputValue;
+        inputArea.classList.remove("error");
+    } else if (/^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/.test(inputValue)) {
+        inputArea.classList.remove("error");
+        domainName = inputValue;
+    } else inputArea.classList.add("error");
     try {
-        const res = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_dfH8tDzTsnI6Fb3ulm1ilJVI7g7qd&ipAddress=" + inputValue);
+        const res = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_dfH8tDzTsnI6Fb3ulm1ilJVI7g7qd&ipAddress=" + ipAddress + "&domain=" + domainName);
         const data = await res.json();
+        updateResult(data);
         const { lat , lng  } = data.location;
         updateMap(lat, lng);
     } catch (error) {
@@ -24,8 +35,8 @@ async function getCoordinates(inputValue) {
 async function updateMap(latitude, longitude) {
     try {
         if (map) map.remove();
-        // const spinner = document.querySelector("#spinner");
-        // spinner.style.display = "block";
+        const spinner = document.querySelector("#spinner");
+        spinner.style.display = "none";
         map = L.map("map").setView([
             latitude,
             longitude
@@ -39,9 +50,18 @@ async function updateMap(latitude, longitude) {
         console.log(error);
     }
 }
-//FORM
-const submitButton = document.querySelector("#submit");
-const inputArea = document.querySelector("input");
+//UPDATE RESULT
+const addressResult = document.querySelector(".result__item--address p");
+const locationResult = document.querySelector(".result__item--location p");
+const timezoneResult = document.querySelector(".result__item--timezone p");
+const ispResult = document.querySelector(".result__item--isp p");
+async function updateResult(data) {
+    addressResult.textContent = data.ip;
+    locationResult.textContent = data.location.city;
+    timezoneResult.textContent = "UTC " + data.location.timezone;
+    ispResult.textContent = data.isp;
+}
+//SUBMIT FORM
 submitButton.addEventListener("click", ()=>{
     getCoordinates(inputArea.value);
 });
